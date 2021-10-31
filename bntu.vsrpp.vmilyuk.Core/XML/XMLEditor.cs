acab0c1bb8c9270d5e6f58fd06fe5de51e41d38a
Милюк Вадим <milyuk.vadim1@mail.ru>
2021-10-31 16:56:25 +0300

    idk wts v2

--- bntu.vsrpp.vmilyuk.Core/XML/XMLEditor.cs
+++ bntu.vsrpp.vmilyuk.Core/XML/XMLEditor.cs
@@ -32,78 +32,95 @@ namespace bntu.vsrpp.vmilyuk.Core.XML
             {
                 foreach (XElement child in node.Elements())
                 {
-                    foreach (string nameChildNode in GetChildNodeNames(xml))
-                    {
-                        if (child.Elements().ToList().Find(x => x.Name.ToString().Equals(nameChildNode)) == null)
-                        {
-                            XElement childNode = GetChildNodeToAddElseReturnsNull(nameChildNode, path);
-                            if (Int32.TryParse(childNode.Value, out int temp))
-                            {
-                                child.Add(new XElement(childNode.Name, 0));
-                            }
-                            else
-                            {
-                                child.Add(new XElement(childNode.Name, "N/A"));
-                            }
-                        }
-                    }
+                    AddNewNodes(child, xml, path);
 
-                    foreach (XElement item in child.Elements())
-                    {
-                        if (item.Name.ToString().Equals("FIO") || item.Name.ToString().Equals("FullName"))
-                        {
-                            var str = item.Value.ToString().Split(' ');
-                            item.Remove();
-                            child.Add(
-                                new XElement("FirstName", str[0]),
-                                new XElement("LastName", str[1]),
-                                new XElement("Surname", str[2])
-                                );
-                        }
-                    } 
-                   
+                    EditNodes(child, xml, path);
                 }
             }
+        }
+
+        private void AddNewNodes(XElement child, XDocument xml, string path)
+        {
+            foreach (string nameChildNode in GetChildNodeNames(xml))
+            {
+                if (child.Elements().ToList().Find(x => x.Name.ToString().Equals(nameChildNode)) == null)
+                {
+                    XElement childNode = GetChildNodeToAddElseReturnsNull(nameChildNode, xml);
+                    if (childNode != null)
+                    {
+                        if (Int32.TryParse(childNode.Value, out int temp))
+                        {
+                            child.Add(new XElement(childNode.Name, 0));
+                        }
+                        else
+                        {
+                            child.Add(new XElement(childNode.Name, "N/A"));
+                        }
+                    }
+                }
+            }
+
             xml.Save(path.Remove(path.Length - FileExtensionLength) + PartOfFileOutputNameWithFileExtension);
         }
 
-        private XElement GetChildNodeToAddElseReturnsNull(string name, string path)
+        private XElement GetChildNodeToAddElseReturnsNull(string name, XDocument xml)
         {
-            XDocument xml = reader.ReadXML(path);
-            foreach (XElement rootNode in xml.Elements())
+            XElement childNode = GetNodeElseReturnsNull(xml).Elements().ToList().Find(x => x.Name.ToString().Equals(name));
+            if (childNode != null)
             {
-                foreach (XElement node in rootNode.Elements())
-                {
-                    XElement childNode = node.Elements().ToList().Find(x => x.Name.ToString().Equals(name));
-                    if (childNode != null)
-                    {
-                        return childNode;
-                    }
-                }
+                return childNode;
             }
+
             return null;
         }
 
-        private List<string> GetChildNodeNames(XDocument xDocument)
+        private List<string> GetChildNodeNames(XDocument xml)
         {
             List<string> childNodeNames = new List<string>();
 
-            foreach (XElement rootNode in xDocument.Elements())
+            foreach (XElement childNode in GetNodeElseReturnsNull(xml).Elements())
             {
-                foreach (XElement node in rootNode.Elements())
+                if ((!childNodeNames.Contains(childNode.Name.ToString())) && !childNode.Name.ToString().Equals("FIO") && !childNode.Name.ToString().Equals("FullName")
+                    && !childNode.Name.ToString().Equals("LastName") && !childNode.Name.ToString().Equals("FirstName") && !childNode.Name.ToString().Equals("Surname"))
                 {
-                    foreach (XElement childNode in node.Elements())
-                    {
-                        if ((!childNodeNames.Contains(childNode.Name.ToString())) && !childNode.Name.ToString().Equals("FIO") && !childNode.Name.ToString().Equals("FullName")
-                            && !childNode.Name.ToString().Equals("LastName") && !childNode.Name.ToString().Equals("FirstName") && !childNode.Name.ToString().Equals("Surname"))
-                        {
-                            childNodeNames.Add(childNode.Name.ToString());
-                        }
-                    }
+                    childNodeNames.Add(childNode.Name.ToString());
                 }
             }
 
             return childNodeNames;
+        }
+
+        private XElement GetNodeElseReturnsNull(XDocument xml)
+        {
+            XElement temp = null;
+            foreach (XElement node in xml.Elements())
+            {
+                foreach (XElement child in node.Elements())
+                {
+                    return child;
+                }
+            }
+
+            return temp;
+        }
+
+        private void EditNodes(XElement child, XDocument xml, string path)
+        {
+            foreach (XElement item in child.Elements())
+            {
+                if (item.Name.ToString().Equals("FIO") || item.Name.ToString().Equals("FullName"))
+                {
+                    var str = item.Value.ToString().Split(' ');
+                    item.Remove();
+                    child.Add(
+                        new XElement("FirstName", str[0]),
+                        new XElement("LastName", str[1]),
+                        new XElement("Surname", str[2])
+                        );
+                }
+            }
+
+            xml.Save(path.Remove(path.Length - FileExtensionLength) + PartOfFileOutputNameWithFileExtension);
         }
     }
 }
